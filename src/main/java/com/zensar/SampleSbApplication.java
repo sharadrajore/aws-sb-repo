5949a69dec35af0924518a823005e761c72f6651
sharad <sharad.rajore@gmail.com>
2021-06-30 12:56:47 +0530

    added sop statement

--- src/main/java/com/zensar/SampleSbApplication.java
+++ src/main/java/com/zensar/SampleSbApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class SampleSbApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hi");
 		SpringApplication.run(SampleSbApplication.class, args);
 	}
 
